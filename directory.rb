9d5e2d95803b8c5fa7e227d62ba0a267e85e42f5
Tom Carmichael <tomcarmichael@hotmail.co.uk>
2023-03-07 16:15:48 +0000

    Add file loading functionality

--- directory.rb
+++ directory.rb
@@ -12,11 +12,13 @@ end
 def process(selection)
   case selection
   when "1"
-    students = input_students
+    input_students
   when "2"
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit # this will cause the program to terminate
   else
@@ -29,6 +31,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
   # 2. read the input and save it into a variable
 end
@@ -55,6 +58,15 @@ def input_students
     # get another name from the user
     name = gets.chomp
   end
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
 end
 
 def print_header
