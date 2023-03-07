7e8368d884af60e34c2874e350984756efb4b0cf
Tom Carmichael <tomcarmichael@hotmail.co.uk>
2023-03-07 10:02:13 +0000

    Refactor to make use of methods

--- directory.rb
+++ directory.rb
@@ -10,11 +10,22 @@ students = [
   "The Joker",
   "Joffrey Baratheon",
   "Norman Bates"]
-#first we print the list of students
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
-# finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
