8c01c136c332b460eda39134792582f6e0f8a442
Tom Carmichael <tomcarmichael@hotmail.co.uk>
2023-03-07 11:03:34 +0000

    Add user interactivity with gets

--- directory.rb
+++ directory.rb
@@ -1,16 +1,21 @@
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :novmeber},
-  {name: "Darth Vader", cohort: :novmeber},
-  {name: "Nurse Ratched", cohort: :novmeber},
-  {name: "Michael Corleone", cohort: :novmeber},
-  {name: "Alex DeLarge", cohort: :novmeber},
-  {name: "The Wicked Witch of the West", cohort: :novmeber},
-  {name: "Terminator", cohort: :novmeber},
-  {name: "Freddy Krueger", cohort: :novmeber},
-  {name: "The Joker", cohort: :novmeber},
-  {name: "Joffrey Baratheon", cohort: :novmeber},
-  {name: "Norman Bates", cohort: :novmeber},
-  ]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -27,6 +32,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
